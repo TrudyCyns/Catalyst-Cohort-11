8ede5f363c7df4ed0badc313341db62094b732eb
mmuwanguzi aaron <aaronmuwanguzi05@gmail.com>
2022-06-03 16:20:41 -0700

    .\Aaronoop.js

---
+++ Aaronoop.js
@@ -0,0 +1,32 @@
+// dynamic functions
+function loop(a,b)
+{
+    var c = a*b;
+    console.log(c);
+}
+loop(20,70) 
+
+function aaron(age,name)
+{
+let age1 = age-10;
+console.log("dear"+ name +"\t" )
+console.log("your new age: \t",age1)
+return age1
+}
+aaron(40, name = "genes")
+
+// static functions
+function count() {
+    for (i = 0; i >= 10; i++);
+        console, log(i);
+}
+
+// object oriented programming
+
+
+var refactory = {
+    elements : "students",
+    Number : 78,
+    teachers : 7,
+    students : "many"
+}
